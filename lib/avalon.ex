ed5d0ed9200186a4e04f784829241cb6513bc5aa
Christopher Grainger <chris@amplified.ai>
2025-02-17 11:43:20 +0100

    Add top level chat with pluggable provider

--- lib/avalon.ex
+++ lib/avalon.ex
@@ -2,17 +2,32 @@ defmodule Avalon do
   @moduledoc """
   Documentation for `Avalon`.
   """
+  alias Avalon.Conversation
+  alias Avalon.Conversation.Message
 
+  @chat_schema [
+    provider: [
+      type: :atom,
+      required: true,
+      doc: "The provider to use for chat completion",
+      type_spec: :module
+    ],
+    provider_opts: [
+      type: :keyword_list,
+      default: [],
+      doc: "Options to pass to the provider"
+    ]
+  ]
   @doc """
-  Hello world.
+  Send a `Conversation.t()` to an LLM and get back a `Message.t()`.
 
-  ## Examples
-
-      iex> Avalon.hello()
-      :world
-
+  ## Options
+  #{NimbleOptions.docs(@chat_schema)}
   """
-  def hello do
-    :world
+  def chat(conversation, opts \\ []) do
+    case NimbleOptions.validate(opts, @chat_schema) do
+      {:ok, opts} -> opts[:provider].chat(conversation, opts[:provider_opts])
+      {:error, error} -> {:error, error}
+    end
   end
 end
--- test/avalon_test.exs
+++ test/avalon_test.exs
@@ -1,8 +1,4 @@
 defmodule AvalonTest do
   use ExUnit.Case
   doctest Avalon
-
-  test "greets the world" do
-    assert Avalon.hello() == :world
-  end
 end
